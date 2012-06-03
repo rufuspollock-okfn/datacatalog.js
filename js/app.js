92e0167a1eef15e90edddb5a316ce0dd8a1ad980
Rufus Pollock <rufus.pollock@okfn.org>
2012-06-03 12:20:09 +0100

    [#2,home][s]: list top groups on front page.

--- js/app.js
+++ js/app.js
@@ -5,5 +5,36 @@ jQuery(document).ready(function($) {
   CKAN.UI.initialize({
     config: localConfig
   });
+
+  var workspace = CKAN.UI.workspace;
+  workspace.client.getTopGroups('openspending', showGroups);
+
+  function showGroups(data) {
+    var groups = _.map(data.result.facets.groups, function(count, key) {
+      return {
+        id: key,
+        count: count
+      };
+    });
+    groups = _.sortBy(groups, function(item) {
+      return -item.count;
+    });
+    var template = ' \
+      <div class="groups"> \
+        <div class="row"> \
+          {{#groups}} \
+          <div class="span4"> \
+            <div class="well"> \
+              <h3>{{id}} {{count}} </h3> \
+            </div> \
+          </div> \
+          {{/groups}} \
+        </div> \
+      </div> \
+    ';
+    var html = Mustache.render(template, {groups: groups});
+    $('#search-page').append(html);
+    $('#search-page').show();
+  }
 });
 
